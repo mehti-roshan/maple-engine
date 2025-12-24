e6566d766d777c9c56d394414272db6d50425d31
mehti <mehti6772@gmail.com>
2025-12-24 15:01:38 +0330

    separate functions for probing info from vulkan

--- engine/renderer/renderer.cpp
+++ engine/renderer/renderer.cpp
@@ -8,9 +8,12 @@ const std::vector<const char*> validationLayers = {
 #endif
 };
 
-VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
-                                      const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger) {
-  auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+VkResult CreateDebugUtilsMessengerEXT(
+    VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
+    const VkAllocationCallbacks* pAllocator,
+    VkDebugUtilsMessengerEXT* pDebugMessenger) {
+  auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(
+      instance, "vkCreateDebugUtilsMessengerEXT");
   if (func != nullptr) {
     return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
   } else {
@@ -18,27 +21,35 @@ VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMes
   }
 }
 
-void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator) {
-  auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+void DestroyDebugUtilsMessengerEXT(VkInstance instance,
+                                   VkDebugUtilsMessengerEXT debugMessenger,
+                                   const VkAllocationCallbacks* pAllocator) {
+  auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(
+      instance, "vkDestroyDebugUtilsMessengerEXT");
   if (func != nullptr) {
     func(instance, debugMessenger, pAllocator);
   }
 }
 
-static VKAPI_ATTR VkBool32 VKAPI_CALL vulkanDebugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
-                                                          VkDebugUtilsMessageTypeFlagsEXT messageType,
-                                                          const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData) {
+static VKAPI_ATTR VkBool32 VKAPI_CALL
+vulkanDebugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+                    VkDebugUtilsMessageTypeFlagsEXT messageType,
+                    const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+                    void* pUserData) {
   MAPLE_DEBUG("Vulkan validation layer: {}", pCallbackData->pMessage);
 
   return VK_FALSE;
 }
 
-void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo) {
+void populateDebugMessengerCreateInfo(
+    VkDebugUtilsMessengerCreateInfoEXT& createInfo) {
   createInfo = {
       .sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT,
-      .messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+      .messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
+                         VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
                          VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT,
-      .messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+      .messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+                     VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
                      VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT,
       .pfnUserCallback = vulkanDebugCallback,
   };
@@ -55,40 +66,26 @@ struct Renderer::Impl {
   std::vector<VkLayerProperties> mAvailableInstanceLayers;
   std::vector<VkPhysicalDevice> mAvailableDevices;
 
-  void Init(const uint32_t requiredExtensionsCount, const char* const* requiredExtensions) {
+  void Init(const uint32_t requiredExtensionsCount,
+            const char* const* requiredExtensions) {
     MAPLE_INFO("Initializing Renderer...");
+    probeInstanceExtensions();
+    probeInstanceLayers();
     createVulkanInstance(requiredExtensionsCount, requiredExtensions);
     setupDebugCallback();
     selectPhysicalDevice();
   }
+
   void Destroy() {
     MAPLE_INFO("Cleaning Renderer...");
-    if (validationLayers.size() > 0) DestroyDebugUtilsMessengerEXT(mVkInstance, mDebugMessenger, nullptr);
+    if (validationLayers.size() > 0)
+      DestroyDebugUtilsMessengerEXT(mVkInstance, mDebugMessenger, nullptr);
 
     vkDestroyInstance(mVkInstance, nullptr);
   }
 
-  void createVulkanInstance(const uint32_t requiredExtensionsCount, const char* const* windowRequiredExtensions) {
-    uint32_t numExtensions = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &numExtensions, nullptr);
-    mAvailableInstanceExtensions.resize(numExtensions);
-    vkEnumerateInstanceExtensionProperties(nullptr, &numExtensions, mAvailableInstanceExtensions.data());
-
-    uint32_t numLayers = 0;
-    vkEnumerateInstanceLayerProperties(&numLayers, nullptr);
-    mAvailableInstanceLayers.resize(numLayers);
-    vkEnumerateInstanceLayerProperties(&numLayers, mAvailableInstanceLayers.data());
-
-    MAPLE_INFO("Available Vulkan instance extensions:");
-    for (const auto& e : mAvailableInstanceExtensions) {
-      MAPLE_INFO("\t{}: {}", e.extensionName, e.specVersion);
-    }
-
-    MAPLE_INFO("Available Vulkan instance layers:");
-    for (const auto& l : mAvailableInstanceLayers) {
-      MAPLE_INFO("\t{}: {}, {}, ({})", l.layerName, l.specVersion, l.implementationVersion, l.description);
-    }
-
+  void createVulkanInstance(const uint32_t requiredExtensionsCount,
+                            const char* const* windowRequiredExtensions) {
     for (const char* layerName : validationLayers) {
       bool found = false;
 
@@ -99,16 +96,22 @@ struct Renderer::Impl {
         }
       }
 
-      if (!found) MAPLE_FATAL("Failed to find required Vulkan instance layer \"{}\"", layerName);
+      if (!found)
+        MAPLE_FATAL("Failed to find required Vulkan instance layer \"{}\"",
+                    layerName);
     }
 
-    std::vector<const char*> requiredExtensions(windowRequiredExtensions, windowRequiredExtensions + requiredExtensionsCount);
-    if (validationLayers.size() > 0) requiredExtensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+    std::vector<const char*> requiredExtensions(
+        windowRequiredExtensions,
+        windowRequiredExtensions + requiredExtensionsCount);
+    if (validationLayers.size() > 0)
+      requiredExtensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
 
     VkApplicationInfo appInfo{};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    appInfo.pApplicationName = "Application name";                 // TODO: replace
-    appInfo.applicationVersion = VK_MAKE_API_VERSION(0, 1, 0, 0);  // TODO: replace
+    appInfo.pApplicationName = "Application name";  // TODO: replace
+    appInfo.applicationVersion =
+        VK_MAKE_API_VERSION(0, 1, 0, 0);  // TODO: replace
     appInfo.pEngineName = "Maple Engine";
     appInfo.engineVersion = VK_MAKE_API_VERSION(0, 1, 0, 0);  // TODO: replace
     appInfo.apiVersion = VK_API_VERSION_1_0;
@@ -121,12 +124,15 @@ struct Renderer::Impl {
         .pNext = validationLayers.size() > 0 ? &debugCreateInfo : nullptr,
         .pApplicationInfo = &appInfo,
         .enabledLayerCount = static_cast<uint32_t>(validationLayers.size()),
-        .ppEnabledLayerNames = validationLayers.size() > 0 ? validationLayers.data() : nullptr,
-        .enabledExtensionCount = static_cast<uint32_t>(requiredExtensions.size()),
+        .ppEnabledLayerNames =
+            validationLayers.size() > 0 ? validationLayers.data() : nullptr,
+        .enabledExtensionCount =
+            static_cast<uint32_t>(requiredExtensions.size()),
         .ppEnabledExtensionNames = requiredExtensions.data(),
     };
 
-    if (vkCreateInstance(&createInfo, nullptr, &mVkInstance) != VK_SUCCESS) MAPLE_FATAL("Failed to create vulkan instance");
+    if (vkCreateInstance(&createInfo, nullptr, &mVkInstance) != VK_SUCCESS)
+      MAPLE_FATAL("Failed to create vulkan instance");
   }
 
   void setupDebugCallback() {
@@ -135,18 +141,45 @@ struct Renderer::Impl {
     VkDebugUtilsMessengerCreateInfoEXT createInfo;
     populateDebugMessengerCreateInfo(createInfo);
 
-    if (CreateDebugUtilsMessengerEXT(mVkInstance, &createInfo, nullptr, &mDebugMessenger) != VK_SUCCESS)
+    if (CreateDebugUtilsMessengerEXT(mVkInstance, &createInfo, nullptr,
+                                     &mDebugMessenger) != VK_SUCCESS)
       MAPLE_FATAL("Failed to create a Vulkan debug messenger");
   }
 
-  void selectPhysicalDevice() {
-    
+  void selectPhysicalDevice() {}
+
+  void probeInstanceExtensions() {
+    uint32_t numExtensions = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &numExtensions, nullptr);
+    mAvailableInstanceExtensions.resize(numExtensions);
+    vkEnumerateInstanceExtensionProperties(nullptr, &numExtensions,
+                                           mAvailableInstanceExtensions.data());
+
+    MAPLE_INFO("Available Vulkan instance extensions ({}):", numExtensions);
+    for (const auto& e : mAvailableInstanceExtensions) {
+      MAPLE_INFO("\t{}: {}", e.extensionName, e.specVersion);
+    }
+  }
+
+  void probeInstanceLayers() {
+    uint32_t numLayers = 0;
+    vkEnumerateInstanceLayerProperties(&numLayers, nullptr);
+    mAvailableInstanceLayers.resize(numLayers);
+    vkEnumerateInstanceLayerProperties(&numLayers,
+                                       mAvailableInstanceLayers.data());
+
+    MAPLE_INFO("Available Vulkan instance layers ({}):", numLayers);
+    for (const auto& l : mAvailableInstanceLayers) {
+      MAPLE_INFO("\t{}: {}, {}, ({})", l.layerName, l.specVersion,
+                 l.implementationVersion, l.description);
+    }
   }
 };  // namespace maple
 
 Renderer::Renderer() : mPimpl(std::make_unique<Impl>()) {}
 Renderer::~Renderer() {}
-void Renderer::Init(const uint32_t requiredExtensionsCount, const char* const* requiredExtensions) {
+void Renderer::Init(const uint32_t requiredExtensionsCount,
+                    const char* const* requiredExtensions) {
   mPimpl->Init(requiredExtensionsCount, requiredExtensions);
 }
 void Renderer::Destroy() { mPimpl->Destroy(); }
