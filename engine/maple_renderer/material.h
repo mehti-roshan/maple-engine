807e57b830e51b7fbbd9b39343452156c8bc36c8
mehti <mehti6772@gmail.com>
2026-07-14 02:48:18 +0330

    remove todo

--- engine/maple_renderer/material.h
+++ engine/maple_renderer/material.h
@@ -7,7 +7,6 @@
 
 namespace maple {
 struct Material {
-  // TODO: store a map of pipelines instead, for when a pipeline is used with different format of attachments
   // since we don't know the output attachment formats of the pipeline
   // we only store the build data and compile lazily when needed
   std::optional<vkm::Pipeline> pipeline = std::nullopt;
